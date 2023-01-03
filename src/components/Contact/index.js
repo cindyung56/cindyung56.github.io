7b3de43bdad79350304adeb535b6b11da5648eef
cindy <cindyung56@gmail.com>
2023-01-03 11:07:03 -0800

    updated resume

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -17,7 +17,7 @@ export default function Contact() {
       <section className="content contact-content" id="contact">
         <h2 className="content-text contact-title">Contact Me!</h2>
         <div className="contact-div">
-          <a href="https://drive.google.com/file/d/1kxZufqrAn_t2WYDDn8wQJT1jD6tOwij1/view?usp=sharing">
+          <a href="https://drive.google.com/file/d/1OeVFoVRKXn9i_JDKsUDlBcIf-t4wlAp-/view?usp=sharing">
             <button className="contact-btn" id="resume">
               <FontAwesomeIcon icon={faFileLines}/>{" "}
               Resume
