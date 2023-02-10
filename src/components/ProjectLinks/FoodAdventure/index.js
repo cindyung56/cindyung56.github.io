5adb3d62dece6781d35320f36393362bba1a2c70
cindy <cindyung56@gmail.com>
2023-02-09 16:28:33 -0800

    started working on layout

--- src/components/ProjectLinks/FoodAdventure/index.js
+++ src/components/ProjectLinks/FoodAdventure/index.js
@@ -2,10 +2,18 @@ import React from "react";
 import "./style.css";
 import { Link } from "react-router-dom";
 
-function FoodAdventure(){
-    return(
-        <div className="project-body">FOOD ADVENTURE</div>
-    )
+export default function FoodAdventure() {
+  return (
+    <div className="project-body">
+      <div className="head-section">
+        <Link to="/" className="go-back">
+          {"<"} BACK
+        </Link>
+        <h1 className="p-heading">FOOD ADVENTURE</h1>
+      </div>
+      <div className="p-description">
+        
+      </div>
+    </div>
+  );
 }
-
-export default FoodAdventure;
